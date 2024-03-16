33bd4c4953b5d7d88edd5caabb293d33f3a11150
Marcos Moraes <contato@marcosamoraes.com>
2024-03-16 19:13:22 -0300

    add PrismaService to appModule

--- paggo-api/src/app.module.ts
+++ paggo-api/src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UploadService } from './upload/upload.service';
 import { UploadController } from './upload/upload.controller';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController, UploadController],
-  providers: [AppService, UploadService],
+  providers: [AppService, UploadService, PrismaService],
 })
 export class AppModule {}
