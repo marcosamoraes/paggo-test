00b1356e4e4508a544dd04e257c47e07eec7ab23
Marcos Moraes <contato@marcosamoraes.com>
2024-03-16 19:10:16 -0300

    prisma service and test

--- paggo-api/src/app.controller.ts
+++ paggo-api/src/app.controller.ts
@@ -9,4 +9,20 @@ export class AppController {
   getServerStatus(): string {
     return this.appService.getServerStatus();
   }
+
+  @Get('users')
+  async getUsers(): Promise<any> {
+    const users = await this.appService.users({});
+    return users;
+  }
+
+  @Get('users/store')
+  async storeUser(): Promise<any> {
+    const user = await this.appService.createUser({
+      email: 'teste@teste.com',
+      name: 'Teste',
+      password: '123456',
+    });
+    return user;
+  }
 }
--- paggo-api/src/app.service.ts
+++ paggo-api/src/app.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
+  constructor(private prisma: PrismaService) {}
+
   getServerStatus(): string {
     return 'Server is running!';
+  }
+
+  async users(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.UserWhereUniqueInput;
+    where?: Prisma.UserWhereInput;
+    orderBy?: Prisma.UserOrderByWithRelationInput;
+  }): Promise<User[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.user.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prisma.user.create({
+      data,
+    });
   }
 }
