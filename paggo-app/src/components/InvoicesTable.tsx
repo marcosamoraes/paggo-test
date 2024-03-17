0eb0b4829d05dd497cb5aaeb2b5cf82793f2d79e
Marcos Moraes <contato@marcosamoraes.com>
2024-03-17 10:12:45 -0300

    table stylish improvement

--- paggo-app/src/components/InvoicesTable.tsx
+++ paggo-app/src/components/InvoicesTable.tsx
@@ -14,6 +14,8 @@ const InvoicesTable: React.FC<any> = ({ invoices, isLoading }: Props) => {
     <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
       <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
         <tr>
+          <th scope="col" className="px-6 py-3">ID</th>
+          <th scope="col" className="px-6 py-3">Status</th>
           <th scope="col" className="px-6 py-3">Invoice Number</th>
           <th scope="col" className="px-6 py-3">Invoice Date</th>
           <th scope="col" className="px-6 py-3">Due Date</th>
@@ -26,6 +28,14 @@ const InvoicesTable: React.FC<any> = ({ invoices, isLoading }: Props) => {
       <tbody>
         {invoices.map((invoice) => (
           <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={invoice.id}>
+            <td className="px-6 py-4">{invoice.id}</td>
+            <td className="px-6 py-4">
+              {invoice.processedAt ? (
+                <p className='bg-green-500 text-green-700 dark:bg-green-400 dark:text-green-800 rounded-full px-2 py-1'>Processed</p>
+              ) : (
+                <p className='bg-yellow-500 text-yellow-700 dark:bg-yellow-400 dark:text-yellow-800 rounded-full px-2 py-1'>Pending</p>
+              )}
+            </td>
             <td className="px-6 py-4">{invoice.invoiceNumber}</td>
             <td className="px-6 py-4">{invoice.invoiceDate ? new Date(invoice.invoiceDate).toLocaleDateString() : '-'}</td>
             <td className="px-6 py-4">{invoice.dueDate ? new Date(invoice.dueDate).toLocaleDateString() : '-'}</td>
