1ebf2cfa0e1caa996cf979f2db678c917adf6a66
Marcos Moraes <contato@marcosamoraes.com>
2024-03-17 13:00:33 -0300

    InvoiceGateway added to AppModule

--- paggo-api/src/app.module.ts
+++ paggo-api/src/app.module.ts
@@ -6,10 +6,17 @@ import { PrismaService } from './prisma.service';
 import { InvoiceController } from './invoice/invoice.controller';
 import { InvoiceService } from './invoice/invoice.service';
 import { UserService } from './user/user.service';
+import { InvoiceGateway } from './invoice/invoice.gateway';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController, InvoiceController],
-  providers: [AppService, PrismaService, InvoiceService, UserService],
+  providers: [
+    AppService,
+    PrismaService,
+    InvoiceService,
+    UserService,
+    InvoiceGateway,
+  ],
 })
 export class AppModule {}
