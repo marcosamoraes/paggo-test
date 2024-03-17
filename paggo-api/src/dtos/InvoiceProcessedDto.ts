c11e28bf9b348f5d64c3c90b1de01392c7702af5
Marcos Moraes <contato@marcosamoraes.com>
2024-03-17 08:15:53 -0300

    fix: invoice processed dto

--- paggo-api/src/dtos/InvoiceProcessedDto.ts
+++ paggo-api/src/dtos/InvoiceProcessedDto.ts
@@ -1,21 +1,26 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class InvoiceProcessedDto {
   @IsNotEmpty()
   file: string;
 
+  @IsOptional()
   @IsString()
   invoiceNumber: string;
 
+  @IsOptional()
   @IsString()
   invoiceDate: string;
 
+  @IsOptional()
   @IsString()
   dueDate: string;
 
+  @IsOptional()
   @IsString()
   balanceDue: number;
 
+  @IsOptional()
   @IsString()
   metadata: string;
 }
